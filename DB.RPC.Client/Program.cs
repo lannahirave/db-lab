2404c1f1951b96e9fe6f5f1150d913d554d7ec86
Vladyslav Tsytsiura <92056657+VladKNU@users.noreply.github.com>
2023-11-28 18:35:09 +0200

    Add RPC Server and basic client

---
+++ DB.RPC.Client/Program.cs
@@ -0,0 +1,11 @@
+﻿using CoreRPC;
+using CoreRPC.Transport.NamedPipe;
+using DB.RPC.Common;
+var transport = new NamedPipeClientTransport("DbPipe");
+
+var proxy = new Engine().CreateProxy<IDbService>(transport);
+var res = await proxy.GetTables();
+foreach (var table in res)
+{
+    Console.WriteLine(table);
+}
