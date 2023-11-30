7be8a7937e3be6baa8ccf6c7fd93b72560303c7b
krolebord <53054887+krolebord@users.noreply.github.com>
2023-11-30 22:36:18 +0200

    Add gRPC client

---
+++ DB.gRPC.Client/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5002");
+var client = new DB.gRPC.Generated.DB.DBClient(channel);
+
+var tables = await client.GetTablesAsync(new Empty());
+
+foreach (var table in tables.Tables)
+{
+    Console.WriteLine(table);
+}
